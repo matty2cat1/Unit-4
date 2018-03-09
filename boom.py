00ba2e2f705d1bb03f5b1de4f24723842ad9e78b
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-03-09 08:45:31 -0500

    Create boom.py

---
+++ boom.py
@@ -0,0 +1,11 @@
+#Matt westelman
+#3/9/18
+#countdown.py - BADABOOM
+
+def boom(a):
+    while a>0:
+        print(a)
+        a=a-1
+    print("Bomb has been defused, Counter terrorists win.")
+boom(40)
+    
