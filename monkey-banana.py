92f5365113c152c62161e659f139bcb810844a97
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-03-23 08:50:04 -0400

    Update monkey-banana.py

--- monkey-banana.py
+++ monkey-banana.py
@@ -1,6 +1,6 @@
 #Matt Westelman
 #3/22/18
-#monkey-banana.py
+#monkey-banana.py - my first game
 
 from ggame import *
 from random import randint
@@ -10,50 +10,57 @@ ROWS = 20
 COLS = 40
 CELL_SIZE = 20
 
-#Move
+#Moves monkey right 1 cell if possible + collects banana
 def moveRight(event):
      if monkey.x < (COLS-1)*CELL_SIZE:
         monkey.x +=CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
             updateScore()
-    
+
+#Moves monkey left 1 cell if possible + collects banana  
 def moveLeft(event):
     if monkey.x> 0:
         monkey.x -=CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
             updateScore()
-    
+
+#Moves monkey up 1 cell if possible + collects banana    
 def moveUp(event):
     if monkey.y> 0:
         monkey.y -=CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
             updateScore()
-    
+
+#Moves monkey down 1 cell if possible + collects banana
 def moveDown(event):
     if monkey.y < (ROWS-1)*CELL_SIZE:
         monkey.y +=CELL_SIZE 
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
             updateScore()
-    
+
+#Banana teleports to a new location    
 def moveBanana():
-    data['frames'] = 0
+    data['frames'] = 0 #reset the tp timer
     banana.x = randint(0,COLS-1)*CELL_SIZE
     banana.y = randint(0,ROWS-1)*CELL_SIZE
-    
+  
+# updating score
 def updateScore():
     data['score'] += 10
     data['scoreText'].destroy() #remove old writing
     scoreBox = TextAsset('Score = '+str(data['score']))
     data['scoreText'] = Sprite(scoreBox,(0,ROWS*CELL_SIZE))
+#Keeps track of how many frames since last teleport, and TPs it after 150 frames
 def step():
     data['frames'] +=1
     if data['frames']%150 == 0:
         moveBanana()
-    
+
+#Sets up and runs game    
 if __name__ == '__main__':
     
     #hold variable in dictionary
@@ -66,6 +73,7 @@ if __name__ == '__main__':
     brown = Color(0x8B4513,1)
     yellow = Color(0xFFFF00,1)
     
+    #graphics for the graphic throne
     jungleBox = RectangleAsset(CELL_SIZE*COLS,CELL_SIZE*ROWS, LineStyle(1,green),green)
     monkeyBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,brown),brown)
     bananaBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,yellow),yellow)
