53336076fdc9d17788070a372434264e0d21ea04
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-03-29 08:11:51 -0400

    Update recursionDemo.py

--- recursionDemo.py
+++ recursionDemo.py
@@ -1,1 +1,12 @@
+#Matt Westelman
+#recursionDemo.py - recursive version of countdown
+#3/29/18
 
+def countdownr(n):
+    if n == 0: #base case
+        print('BOOM!') 
+    else:
+        print(n)
+        countdownr(n-1)
+
+countdownr(10)
